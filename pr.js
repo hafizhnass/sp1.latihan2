6d9dc01a4b553e621663f69ad8d83f6f7d6a7bfb
hafizhnass <87346700+hafizhnass@users.noreply.github.com>
2021-07-22 12:51:31 +0700

    Add files via upload

---
+++ pr.js
@@ -0,0 +1,7 @@
+var nama = prompt ("masukkan nama pelajar")
+var nilai = prompt ("input nilai anda")
+if(nilai > 70) {
+    document.write( nama + "selamat anda lulus cukup memuaskan dengan nominal nilai" + ' : ' + nilai)
+} else {
+    document.write( nama + "anda tidak lulus karena anda kurang biaya nyogok dengan nominal nilai" + ' : ' + nilai)
+}
